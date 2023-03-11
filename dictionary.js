448c659ccd61bb77784249224fa022e477b8f038
David W. Farmer <farmer@aimath.org>
2023-03-11 11:42:35 -0500

    add greek letters

--- dictionary.js
+++ dictionary.js
@@ -2043,3 +2043,95 @@ var symbolswithlimits = {
 // need to generate this automatically from: integrals, symbolswithlimits, ???
 
 var operatorsymbols = [ "∑","⋃","⋂","⨁","⨂","∐","∏","∮","∭","∬","∫","∰","∯","∮"];
+
+var greekletters = [["α","alpha"],
+["β","beta"],
+["γ","gamma"],
+["δ","delta"],
+["ε","epsilon"],
+["ζ","zeta"],
+["η","eta"],
+["θ","theta"],
+["ι","iota"],
+["κ","kappa"],
+["λ","lambda"],
+["μ","mu"],
+["ν","nu"],
+["ξ","xi"],
+["ο","omicron"],
+["π","pi"],
+["ρ","rho"],
+["σ","sigma"],
+["τ","tau"],
+["υ","upsilon"],
+["φ","phi"],
+["χ","chi"],
+["ψ","psi"],
+["ω","omega"],
+["Α","Alpha"],
+["Β","Beta"],
+["Γ","Gamma"],
+["Δ","Delta"],
+["Ε","Epsilon"],
+["Ζ","Zeta"],
+["Η","Eta"],
+["Θ","Theta"],
+["Ι","Iota"],
+["Κ","Kappa"],
+["Λ","Lambda"],
+["Μ","Mu"],
+["Ν","Nu"],
+["Ξ","Xi"],
+["Ο","Omicron"],
+["Π","Pi"],
+["Ρ","Rho"],
+["Σ","Sigma"],
+["Τ","Tau"],
+["Υ","Upsilon"],
+["Φ","Phi"],
+["Χ","Chi"],
+["Ψ","Psi"],
+["Ω","Omega"]];
+
+for (const letterpair of greekletters) {
+  dictionary[letterpair[1]] = {
+    "alternative": [],
+    "type": "symbol",
+    "priority": -1,
+    "rule": {
+      "1,1": "\\" + letterpair[1]
+    },
+    "speech": {
+      "1,1": " " + letterpair[1] + " "
+    },
+    "ruleML": {
+      "1,1": "<mi>" + letterpair[0] + "</mi>"
+    }
+  }
+}
+/*
+["Α","Alpha"],
+["Β","Beta"],
+["Γ","Gamma"],
+["Δ","Delta"],
+["Ε","Epsilon"],
+["Ζ","Zeta"],
+["Η","Eta"],
+["Θ","Theta"],
+["Ι","Iota"],
+["Κ","Kappa"],
+["Λ","Lambda"],
+["Μ","Mu"],
+["Ν","Nu"],
+["Ξ","Xi"],
+["Ο","Omicron"],
+["Π","Pi"],
+["Ρ","Rho"],
+["Σ","Sigma"],
+["Τ","Tau"],
+["Υ","Upsilon"],
+["Φ","Phi"],
+["Χ","Chi"],
+["Ψ","Psi"],
+["Ω","Omega"],
+*/
